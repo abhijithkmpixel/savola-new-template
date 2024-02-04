0b544a040d1ad2e8a379581f921f2cb809db3c5c
abhijith <abhijith@pixelflames.com>
2024-02-04 19:37:54 +0530

    new changees

--- main.js
+++ main.js
@@ -25,29 +25,68 @@ $(document).ready(function () {
       $('.disclaimer__popup__outer').remove();
     })
   }
+  $('.overview__slider__mutli').slick({
+    infinite: false,
+    slidesToShow: 3,
+    rtl: $('html').attr('dir') === 'rtl', // Use jQuery to get the 'dir' attribute
+    slidesToScroll: 1, // Keep slidesToScroll positive regardless of RTL
+    nextArrow: `<button class="slick__btn slick__nxt">
+      <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="0.969824" y1="10.3314" x2="20.2187" y2="10.3314" stroke="#114D69" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12.9006 1.67871L21.5978 10.4383L12.9006 19.198" stroke="#114D69" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </button>`,
+    prevArrow: `<button class="slick__btn slick__prev">
+      <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 21" width="23" height="21">
+        <title>icons-svg</title>
+        <style>
+          .s0 { fill: none;stroke: #114d69;stroke-linecap: round;stroke-linejoin: round;stroke-width: 1.8 } 
+        </style>
+        <path id="Layer" class="s0" d="m22.6 10.9h-19.2"/>
+        <path id="Layer" fill-rule="evenodd" class="s0" d="m10.7 19.5l-8.7-8.7 8.7-8.8"/>
+      </svg>
+    </button>`,
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+
+  });
+  $('.overview__slider').slick({
+    infinite: false,
+    slidesToShow: 1,
+    rtl: $('html').attr('dir') === 'rtl', // Use jQuery to get the 'dir' attribute
+    slidesToScroll: 1, // Keep slidesToScroll positive regardless of RTL
+    nextArrow: `<button class="slick__btn slick__nxt">
+      <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="0.969824" y1="10.3314" x2="20.2187" y2="10.3314" stroke="#114D69" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12.9006 1.67871L21.5978 10.4383L12.9006 19.198" stroke="#114D69" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+    </button>`,
+    prevArrow: `<button class="slick__btn slick__prev">
+      <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 21" width="23" height="21">
+        <title>icons-svg</title>
+        <style>
+          .s0 { fill: none;stroke: #114d69;stroke-linecap: round;stroke-linejoin: round;stroke-width: 1.8 } 
+        </style>
+        <path id="Layer" class="s0" d="m22.6 10.9h-19.2"/>
+        <path id="Layer" fill-rule="evenodd" class="s0" d="m10.7 19.5l-8.7-8.7 8.7-8.8"/>
+      </svg>
+    </button>`,
+  
+  });
+ 
 })
-$('.overview__slider').slick({
-  infinite: false,
-  slidesToShow: 1,
-  rtl: $('html').attr('dir') === 'rtl', // Use jQuery to get the 'dir' attribute
-  slidesToScroll: 1, // Keep slidesToScroll positive regardless of RTL
-  nextArrow: `<button class="slick__btn slick__nxt">
-    <svg width="23" height="21" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <line x1="0.969824" y1="10.3314" x2="20.2187" y2="10.3314" stroke="#114D69" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M12.9006 1.67871L21.5978 10.4383L12.9006 19.198" stroke="#114D69" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-  </button>`,
-  prevArrow: `<button class="slick__btn slick__prev">
-    <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 21" width="23" height="21">
-      <title>icons-svg</title>
-      <style>
-        .s0 { fill: none;stroke: #114d69;stroke-linecap: round;stroke-linejoin: round;stroke-width: 1.8 } 
-      </style>
-      <path id="Layer" class="s0" d="m22.6 10.9h-19.2"/>
-      <path id="Layer" fill-rule="evenodd" class="s0" d="m10.7 19.5l-8.7-8.7 8.7-8.8"/>
-    </svg>
-  </button>`
-});
 setTimeout(() => {
 
   $('.frequentlty__asked__questions__row .faq__wrap p').each(function () {
